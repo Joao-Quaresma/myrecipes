4b3ad3c3de6b71fd486ae2405ef399a3b4d0be0b
JoaoQuaresma <joao.quaresma1991@gmail.com>
2018-09-07 08:53:22 +0100

    add chef and recipe validations with tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@
 # Recipe model
 class Recipe < ApplicationRecord
   validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 500}
 end
---
+++ test/models/chef_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  def setup
+    @chef = Chef.new(chefname: "Joao", email: "joao@example.com")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "name should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "name should less than 30 chars" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "email should be too long" do
+    @chef.email = "a" * 245 + "@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "email should accept correct format" do
+    valid_emails = %w[user@example.com JOAO@gmail.com M.first@yahoo.ca john+smith@co.uk.org]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid format" do
+    invalid_emails = %w[joao@example joao@example,com joao.name@gmail. joe@bar+foo.com]
+    invalid_emails.each do |invalids|
+      @chef.email = invalids
+      assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+
+  test "emails should be case lowercase before hitting db" do
+    mixed_email = "John@example.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+end
