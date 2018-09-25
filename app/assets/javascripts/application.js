e2866c91a4549e4e549009e7654fb60fca86dd75
JoaoQuaresma <joao.quaresma1991@gmail.com>
2018-09-25 18:20:24 +0100

    fix dropdown 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery.turbolinks
 //= require jquery
+//= require rails-ujs
 //= require jquery-ui
 //= require bootstrap
 //= require bootstrap-sprockets
